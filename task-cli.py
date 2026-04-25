a3460d30103fc7b18bd639433f2f0f9ad68dd6ba
Toby Frost <tobycfrost14@gmail.com>
2026-04-25 13:32:22 +1000

    Implemented update command

--- task-cli.py
+++ task-cli.py
@@ -14,8 +14,13 @@ def add_task(args):
     save_tasks()
     print(f"Task added successfully (ID: {len(tasks)})")
 
-def update_task(id, description):
-    pass
+def update_task(args):
+    try:
+        tasks[args.id - 1].description = args.description
+        save_tasks()
+        print(f"Task updated successfully (ID: {args.id})")
+    except IndexError:
+        print("That id doesn't exist type 'list' to see the ids of all tasks.")
 
 def delete_task(id):
     pass
@@ -57,23 +62,23 @@ add_parser.set_defaults(func=add_task)
 
 # update command
 update_parser = subparsers.add_parser("update", help="Updates the description of a task")
-update_parser.add_argument("id", help="The unique identifier for the task")
+update_parser.add_argument("id", help="The unique identifier for the task", type=int)
 update_parser.add_argument("description", help="A short description of the task")
 update_parser.set_defaults(func=update_task)
 
 # delete command
 delete_parser = subparsers.add_parser("delete", help="Deletes a task")
-delete_parser.add_argument("id", help="The unique identifier for the task")
+delete_parser.add_argument("id", help="The unique identifier for the task", type=int)
 delete_parser.set_defaults(func=delete_task)
 
 # mark-in-progress command
 mark_in_progress_parser = subparsers.add_parser("mark-in-progress", help="Changes a task's status to in-progress")
-mark_in_progress_parser.add_argument("id", help="The unique identifier for the task")
+mark_in_progress_parser.add_argument("id", help="The unique identifier for the task", type=int)
 mark_in_progress_parser.set_defaults(func=mark_task_in_progress)
 
 # mark-done command
 mark_done_parser = subparsers.add_parser("mark-done")
-mark_done_parser.add_argument("id", help="The unique identifier for the task")
+mark_done_parser.add_argument("id", help="The unique identifier for the task", type=int)
 mark_done_parser.set_defaults(func=mark_task_done)
 
 # list command
